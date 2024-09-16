04c3b5765c03d396a816aa32739b980ae72f183a
Faseeki Temitope <65563185+faseeki1@users.noreply.github.com>
2024-09-16 13:41:33 +0100

    Update register.php

--- api/register.php
+++ api/register.php
@@ -2,7 +2,7 @@
 /**
  * Register a new user in the system.
  * 
- * @param string $_POST['username'] The username of the new user.
+ * @param string $_POST['name'] The name of the new user.
  * @param string $_POST['email'] The email address of the new user.
  * @param string $_POST['password'] The password of the new user.
  * 
